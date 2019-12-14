d54765e28ad971276ecd3aae67ae6296ca9336f8
Shubus Corporation <shubus.corporation@gmail.com>
2019-12-14 14:12:07 +0300

    Initial Commit (c) Shubus Corporation

---
+++ 08.03.2014/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace _08._03._2014
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.myTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " ";
+            this.label1.Visible = false;
+            this.label1.TextChanged += new System.EventHandler(this.label_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " ";
+            this.label2.Visible = false;
+            this.label2.TextChanged += new System.EventHandler(this.label_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(61, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " ";
+            this.label3.Visible = false;
+            this.label3.TextChanged += new System.EventHandler(this.label_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(61, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 38);
+            this.label4.TabIndex = 3;
+            this.label4.Tag = "L4";
+            this.label4.Text = " ";
+            this.label4.Visible = false;
+            this.label4.TextChanged += new System.EventHandler(this.label_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(107, 142);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Поздравляем с 8 Марта!";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer myTimer;
+
+    }
+}
+
---
+++ 08.03.2014/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace _08._03._2014
+{
+    public partial class Form1 : Form
+    {
+        private class Decart // Tuple class is available since NET 4.0 
+        {
+            public int xx;
+            public int yy;
+        }
+
+        int scrW = 0;
+        int scrH = 0;
+        int pictCount = 0;
+
+        const int ID_BLINK_COUNT = 3;
+        readonly string IDS_TAG_BLINK = "L4";
+        Dictionary<Label, string> dict = null;
+        delegate void AddFlowerDelegate();
+        delegate void AddFlowerXYDelegate(Decart dec);
+        delegate void AddTextDelegate(Label lb, char ch);
+
+        AddFlowerDelegate addFlower;
+        AddFlowerXYDelegate addFlowerXY;
+        AddTextDelegate addTextDelegate = new AddTextDelegate((l, ch) => l.Text += ch );
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.addFlower = new AddFlowerDelegate(this.addRandomFlower);
+            this.addFlowerXY = new AddFlowerXYDelegate(this.addRandomFlowerXY);
+
+            this.dict = new Dictionary<Label, string>()
+            {
+               { this.label1, "Наши милые лииингвистки, ..." },
+               { this.label2, "Вы мудры как совы и прекрасны как бабочки..." },
+               { this.label3, "Поздравляем вас со светлым праздником Весны 8 Марта !!!" },
+               { this.label4, "Ваши програаааммисты! :)" }
+            };
+        }
+
+        private void setLabelX(Label lb)
+        {
+            int lw = TextRenderer.MeasureText(lb.Text, lb.Font).Width;
+            int sw = this.scrW;
+
+            if (lb.Tag == null)
+            {
+                lb.Location = new Point((sw - lw) / 2, lb.Location.Y);
+            }
+            else if (string.Equals((string)lb.Tag, IDS_TAG_BLINK))
+            {
+                int l3w = TextRenderer.MeasureText(label3.Text, lb.Font).Width;
+                int l3x = this.label3.Location.X;
+                lb.Location = new Point(l3x + (l3w - lw), lb.Location.Y);
+            }
+        }
+
+        private void label_TextChanged(object sender, EventArgs e)
+        {
+            this.setLabelX((Label)sender);
+            ((Label)sender).BringToFront();
+        }
+
+        static int staticCount = 0;
+
+        private void timerEventShow(Object myObject, EventArgs myEventArgs)
+        {
+            this.myTimer.Stop();
+            Random random = new Random();
+            staticCount++;
+
+            foreach(Control c in this.Controls)
+            {
+                if (c is Label)
+                {
+                    if (string.Equals((string)c.Tag, IDS_TAG_BLINK))
+                    {
+                        if (staticCount == ID_BLINK_COUNT)
+                        {
+                            c.Visible = !c.Visible;
+                            staticCount = 0;
+                        }
+                    }
+                    else
+                    {
+                        c.ForeColor = Color.FromArgb(random.Next(byte.MaxValue), random.Next(byte.MaxValue), random.Next(byte.MaxValue));
+                    }
+                }
+            }
+            myTimer.Enabled = true;
+        }
+
+        private Random flRnd = new Random();
+
+        private int getRandom(int x1, int x2)
+        {
+            return flRnd.Next(x1, x2);
+        }
+
+        private string GetPictureName()
+        {
+            return "pbox" + (++this.pictCount);
+        }
+
+        private Random imgRnd = new Random();
+
+        private System.Drawing.Bitmap randomImage
+        {
+            get
+            {
+                string[] images =
+                {
+                    "_1_images", "_2_images", "_3_images", "_4_images",
+                    "images", "images1", "images3", "images4", "_5_images",
+                };
+                object obj = _08._03._2014.Properties.Resources.ResourceManager.GetObject(images[imgRnd.Next(0, images.Length)]);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        private void addRandomFlower()
+        {
+            try
+            {
+                PictureBox rnd = new System.Windows.Forms.PictureBox();
+                rnd.Image = this.randomImage;
+                rnd.Name = GetPictureName();
+
+                int imY = rnd.Image.Size.Height;
+                int imX = rnd.Image.Size.Width;
+
+                rnd.Location = new System.Drawing.Point(getRandom(-imX, scrW), getRandom(-imY, scrH));
+                rnd.Size = new System.Drawing.Size(imX, imY);
+                this.Controls.Add(rnd);
+                rnd.BringToFront();
+            }
+            catch { }
+        }
+
+        private void addRandomFlowerXY(Decart dec)
+        {
+            try
+            {
+                PictureBox rnd = new System.Windows.Forms.PictureBox();
+                rnd.Image = this.randomImage;
+                rnd.Name = GetPictureName();
+
+                int imY = rnd.Image.Size.Height;
+                int imX = rnd.Image.Size.Width;
+
+                rnd.Location = new System.Drawing.Point(dec.xx, dec.yy);
+                rnd.Size = new System.Drawing.Size(imX, imY);
+                this.Controls.Add(rnd);
+
+                dec.xx = dec.xx + imX;
+            }
+            catch { }
+        }
+
+        private void ThreadEntry()
+        {
+            this.scrW = Screen.PrimaryScreen.WorkingArea.Width;
+            this.scrH = Screen.PrimaryScreen.WorkingArea.Height;
+
+            Invoke(new AddFlowerDelegate(() => Cursor.Hide()));
+            Decart dec = new Decart();
+            Object[] obj = new Object[] { dec };
+
+            for (dec.yy = 0; dec.yy < scrH; dec.yy += 100)
+            {
+                for (dec.xx = 0; dec.xx < scrW;)
+                {
+                    Invoke(this.addFlowerXY, obj);
+                    System.Threading.Thread.Sleep(10);
+                }
+            }
+            for (int fl = 0; fl < 250; fl++)
+            {
+                Invoke(this.addFlower);
+                System.Threading.Thread.Sleep(10);
+            }
+
+            Invoke(new AddFlowerDelegate(() =>
+                {
+                    int lh = TextRenderer.MeasureText(this.label1.Text, label1.Font).Height * 2;
+                    int sh = Screen.PrimaryScreen.WorkingArea.Height / 4;
+
+                    this.label1.Location = new Point(label1.Location.X, sh);
+                    this.label2.Location = new Point(label1.Location.X, sh + lh);
+                    this.label3.Location = new Point(label1.Location.X, sh + lh * 2);
+                    this.label4.Location = new Point(label1.Location.X, sh + lh * 4);
+                }
+                ));
+
+            AddTextDelegate showLabel = new AddTextDelegate((lb, x) =>
+                {
+                    lb.Visible = true;                    
+                }
+            );
+
+            foreach (KeyValuePair<Label, string> pair in this.dict)
+            {
+                Invoke(showLabel, new Object[] { pair.Key, ' ' });
+
+                for (int i = 0; i < pair.Value.Length; i++)
+                {
+                    Invoke(this.addTextDelegate, new Object[]{ pair.Key, pair.Value[i] });
+                    System.Threading.Thread.Sleep(50);
+                }
+            }
+            Invoke(new AddFlowerDelegate(() =>
+                {
+                    Cursor.Show();
+                    this.myTimer.Tick += this.timerEventShow;
+                    this.myTimer.Interval = 100;
+                    this.myTimer.Start();
+                }
+                ));
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            new Thread(ThreadEntry){ IsBackground = true }.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                case Keys.Enter:
+                case Keys.Escape:
+                    this.Close();                
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
